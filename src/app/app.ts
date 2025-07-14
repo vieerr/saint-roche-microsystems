60762935092ef5f4046566108e09ff71d060ac72
vieerr <olivier_paspuel@hotmail.com>
2025-07-13 19:40:33 -0500

    feat: footer component added

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
 import { Home } from "./components/home/home";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Navbar, Home],
+  imports: [RouterOutlet, Navbar, Navbar, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
