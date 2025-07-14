3b7a47b503634df7f70ecdeda0cb29cecf4c3049
vieerr <olivier_paspuel@hotmail.com>
2025-07-14 09:01:13 -0500

    feat:ui improvementes

--- src/app/components/contact-us/contact-us.ts
+++ src/app/components/contact-us/contact-us.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLocation, faMailBulk, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './contact-us.html',
-  styleUrl: './contact-us.css'
+  styleUrl: './contact-us.css',
 })
 export class ContactUs {
-
+  constructor() {
+    library.add(faLocation, faPhone, faMailBulk);
+  }
+  faLocation = faLocation;
+  faPhone = faPhone;
+  faMailBulk = faMailBulk;
 }
