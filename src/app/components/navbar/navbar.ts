0b7fdcc635680059af758e75044b1c6cd526d4fb
vieerr <olivier_paspuel@hotmail.com>
2025-07-14 06:36:10 -0500

    feat: responsive support added for navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -2,14 +2,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, RouterModule],
+  imports: [FontAwesomeModule, CommonModule, RouterModule],
   templateUrl: './navbar.html',
-  styleUrls: ['./navbar.css']
+  styleUrls: ['./navbar.css'],
 })
 export class Navbar {
+  constructor() {
+    library.add(faHamburger);
+  }
+  faHamburger = faHamburger;
   isMenuOpen = false;
 
   toggleMenu() {
