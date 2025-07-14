b471bd19b9c7ba4c464edc81f4e6ef95fff35e3a
vieerr <olivier_paspuel@hotmail.com>
2025-07-13 20:52:12 -0500

    feat: team page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { AboutUs } from './components/about-us/about-us';
+import { Team } from './components/team/team';
+import { ContactUs } from './components/contact-us/contact-us';
 
 export const routes: Routes = [
-    { path: '', component: Home },
-    { path: 'about-us', component: AboutUs }
+  { path: '', component: Home },
+  { path: 'about-us', component: AboutUs },
+  { path: 'team', component: Team },
+  { path: 'contact-us', component: ContactUs }
 ];
